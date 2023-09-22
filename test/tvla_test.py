871ff19a83fcc28d65c2acc2b6630756298d6da8
Vladimir Rozic <vrozic@lowrisc.org>
2023-09-22 15:51:42 +0100

    [tvla] Change default value for general-tast
    At the moment the default value of the `--general-test` parameter
    is `True`. This argument cannot be changed to `False` via command
    line, but only through the yaml configuration file.
    Since we don't use configuaration files for TVLA in the CI, it is
    currently impossible to add tests for aes-specific TVLA.
    To enable adding these tests, this commit changes the default value
    to `False` and adds `--general-test` to CI commands where needed.
    
    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>

--- test/tvla_test.py
+++ test/tvla_test.py
@@ -43,7 +43,7 @@ def ttest_compare_results(expected, received, delta) -> bool:
 def test_general_kmac_nonleaking_project():
     project_path = TestDataPath('tvla_general/ci_opentitan_simple_kmac.cwp')
     tvla = TvlaCmd(Args(['--project-file', str(project_path),
-                         '--mode', 'kmac', '--save-to-disk-ttest',
+                         '--mode', 'kmac', '--save-to-disk-ttest', '--general-test',
                          '--number-of-steps', '10', 'run-tvla'])).run()
     expected_path = TestDataPath('tvla_general/ttest-step-golden-kmac.npy.npz')
     expected_file = np.load(str(expected_path))
@@ -61,7 +61,7 @@ def test_general_kmac_nonleaking_project():
 def test_general_aes_nonleaking_project():
     project_path = TestDataPath('tvla_general/ci_opentitan_simple_aes_fvsr.cwp')
     tvla = TvlaCmd(Args(['--project-file', str(project_path),
-                         '--mode', 'aes', '--save-to-disk-ttest',
+                         '--mode', 'aes', '--save-to-disk-ttest', '--general-test',
                          '--number-of-steps', '10', 'run-tvla'])).run()
     expected_path = TestDataPath('tvla_general/ttest-step-golden-aes.npy.npz')
     expected_file = np.load(str(expected_path))
@@ -76,7 +76,8 @@ def test_general_aes_nonleaking_project():
 def test_general_leaking_histogram():
     hist_path = TestDataPath('tvla_general/kmac_hist_leaking.npz')
     tvla = TvlaCmd(Args(['--input-histogram-file', str(hist_path),
-                        '--mode', 'kmac', '--save-to-disk-ttest', 'run-tvla'])).run()
+                        '--mode', 'kmac', '--save-to-disk-ttest', '--general-test',
+                         'run-tvla'])).run()
     assert ttest_significant(np.load('tmp/ttest.npy')), (
            f"{tvla} did not find significant leakage, which is unexpected")
 
@@ -84,6 +85,7 @@ def test_general_leaking_histogram():
 def test_general_nonleaking_histogram():
     hist_path = TestDataPath('tvla_general/kmac_hist_nonleaking.npz')
     tvla = TvlaCmd(Args(['--input-histogram-file', str(hist_path),
-                        '--mode', 'kmac', '--save-to-disk-ttest', 'run-tvla'])).run()
+                        '--mode', 'kmac', '--save-to-disk-ttest', '--general-test',
+                         'run-tvla'])).run()
     assert not ttest_significant(np.load('tmp/ttest.npy')), (
            f"{tvla} did find significant leakage, which is unexpected")

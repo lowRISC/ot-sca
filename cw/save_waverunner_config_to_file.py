f952a77b70c0277b1d050fb69071e5a78415ba7c
{Johann Heyszl} <johannheyszl@google.com>
2023-09-25 19:31:45 +0200

    added file to save config as standalone, to be used in test
    Signed-off-by: {Johann Heyszl} <johannheyszl@google.com>

---
+++ cw/save_waverunner_config_to_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Save WaveRunner to file for later use by script."""
+
+from waverunner import WaveRunner
+
+if __name__ == '__main__':
+    waverunner = WaveRunner("172.26.111.125")
+
+    # Save WaveRunner setup to timestamped file
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d_%H:%M:%S")
+    file_name_local = f"scope_config_{now_str}.lss"
+    waverunner.save_setup_to_local_file(file_name_local)

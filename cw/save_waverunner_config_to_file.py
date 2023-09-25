c60d627aa400c8a5b25560d232cc144c6e7a96ce
{Johann Heyszl} <johannheyszl@google.com>
2023-09-25 21:46:16 +0200

    Rework WaveRunner to support manual scope config w/o hard-coded defaults
    Signed-off-by: {Johann Heyszl} <johannheyszl@google.com>

--- cw/save_waverunner_config_to_file.py
+++ cw/save_waverunner_config_to_file.py
@@ -5,6 +5,8 @@
 
 """Save WaveRunner to file for later use by script."""
 
+from datetime import datetime
+
 from waverunner import WaveRunner
 
 if __name__ == '__main__':
@@ -14,4 +16,7 @@ if __name__ == '__main__':
     now = datetime.now()
     now_str = now.strftime("%Y-%m-%d_%H:%M:%S")
     file_name_local = f"scope_config_{now_str}.lss"
+    waverunner.save_setup_to_local_file(file_name_local)
+
+    file_name_local = "scope_config.lss"
     waverunner.save_setup_to_local_file(file_name_local)

352aaf420124589e8cb251a28d464c2a7942e92e
Siemen Dhooghe <sdhooghe@google.com>
2025-06-20 14:26:09 +0200

    [comm] Add new alert config
    Add the new alert config to enable alerts.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- communication/fi_otbn_commands.py
+++ communication/fi_otbn_commands.py
@@ -206,7 +206,7 @@ class OTFIOtbn:
         self.target.write(json.dumps(parameters).encode("ascii"))
         parameters = {"sensor_ctrl_enable": True, "sensor_ctrl_en_fatal": [False, False, False, False, False, False, False, False, False, False, False]}
         self.target.write(json.dumps(parameters).encode("ascii"))
-        parameters = {"alert_classes":[2,2,2,2,0,0,2,2,2,2,0,0,0,0,0,1,0,0,0,2,2,2,0,0,0,1,0,2,2,2,2,0,1,0,0,1,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,1,0,1,0,1,0,0,0,0,1,0,1], "accumulation_threshold": 2, "signals": [4294967295, 0, 2, 3], "duration_cycles": [0, 2400000,48,48], "ping_timeout": 1200}
+        parameters = {"alert_classes":[2,2,2,2,0,0,2,2,2,2,0,0,0,0,0,1,0,0,0,2,2,2,0,0,0,1,0,2,2,2,2,0,1,0,0,1,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,1,0,1,0,1,0,0,0,0,1,0,1], , "enable_alerts": [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True], "enable_classes": [True,True,False,False], "accumulation_thresholds": [2,2,2,2], "signals": [4294967295, 0, 2, 3], "duration_cycles": [0, 2400000,48,48], "ping_timeout": 1200}
         self.target.write(json.dumps(parameters).encode("ascii"))
         device_id = self.read_response()
         sensors = self.read_response()
